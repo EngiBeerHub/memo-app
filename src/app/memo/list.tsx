ac181c20f13c5a446109054c6826e36c8c395625
EngiBeerHub <engibeer0422@gmail.com>
2025-08-21 22:28:19 +0900

    [10-61]split component

---
+++ src/app/memo/list.tsx
@@ -0,0 +1,33 @@
+import {StyleSheet, View} from "react-native";
+import Header from "../../components/Header";
+import MemoListItem from "../../components/MemoListItem";
+import CircleButton from "../../components/CircleButton";
+
+const Index = () => {
+  return (
+    <View style={styles.container}>
+      {/*header*/}
+      <Header/>
+
+      {/*list*/}
+      <View>
+        {/*item*/}
+        <MemoListItem/>
+        <MemoListItem/>
+        <MemoListItem/>
+      </View>
+
+      {/*FAB*/}
+      <CircleButton>+</CircleButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  }
+});
+
+export default Index;
