6950bb28f1e3d9bfef53728bf6ea6f1b3f251a41
EngiBeerHub <engibeer0422@gmail.com>
2025-08-24 16:03:05 +0900

    [13-96]save data

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,12 +1,21 @@
-import {StyleSheet, Text, TouchableOpacity} from "react-native";
+import {Alert, StyleSheet, Text, TouchableOpacity} from "react-native";
+import {signOut} from 'firebase/auth';
+import {auth} from "../config";
+import {router} from "expo-router";
+
+const handlePress = (): void => {
+  signOut(auth)
+    .then(() => router.replace('/auth/log_in'))
+    .catch(() => Alert.alert('ログアウトに失敗しました'));
+};
 
 const LogOutButton = () => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={handlePress}>
       <Text style={styles.text}>ログアウト</Text>
     </TouchableOpacity>
   );
-}
+};
 
 const styles = StyleSheet.create({
   text: {
@@ -14,6 +23,6 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     color: 'rgba(255, 255, 255, 0.7)'
   }
-})
+});
 
 export default LogOutButton;
